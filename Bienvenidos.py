a262c7de9408f4a8a94a16a9c99ad29118cd3123
Fernando Martinez <fernandomartinezG1D@gmail.com>
2024-05-20 21:17:04 -0600

    Actualizar Bienvenidos.py

--- Bienvenidos.py
+++ Bienvenidos.py
@@ -1,3 +1,3 @@
 print("Bienvenidos al repositorio de Curly")
 print("Muy querido entre la comunidad soft12")
-pronto("Soy Fer xd, un saludo a la curlygang")
+print("Soy Fer xd, un saludo a la curlygang")
